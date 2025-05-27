7a257e356a046a7f2280ba778281807ad9eb101a
3viil <48308226+3viil@users.noreply.github.com>
2025-05-27 09:13:24 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -27,34 +27,22 @@ df_week['Climb/distance ratio'] = df_week['Ascent (feet)'] / df_week['Distance (
 df_week2 = df_week[['Week','Ascent (feet)','Calories']]
 
 
-st.header(":blue[Cyclemeter data since going batshit for MTB]")
+st.header(":blue[Cyclemeter MTB Data, 2011-2025]")
 
+st.subheader("Data by Year")
+tab1, tab2, tab3 = st.tabs(["Ascent & Calories", "Ascent/distance ratio", "Dataframe"])
+tab1.line_chart(df_year2, x="Year", height=400)
+tab2.line_chart(df_year, x="Year", y = "Climb/distance ratio", height=400)
+tab3.dataframe(df_year, height=400, use_container_width=True)
 
-st.subheader("Feet climbed and calories burned by year")
-st.line_chart(df_year2, x="Year")
+st.subheader("Data by Month")
+tab1, tab2, tab3 = st.tabs(["Ascent & Calories", "Ascent/distance ratio", "Dataframe"])
+tab1.line_chart(df_month2, x="Month", height=400)
+tab2.line_chart(df_month, x="Month", y = "Climb/distance ratio", height=400)
+tab3.dataframe(df_month, height=400, use_container_width=True)
 
-st.subheader("Ascent/distance ratio by year")
-st.line_chart(df_year, x="Year", y = "Climb/distance ratio")
-
-
-st.subheader("Feet climbed and calories burned by month")
-st.line_chart(df_month2, x="Month")
-
-st.subheader("Ascent/distance ratio by month")
-st.line_chart(df_month, x="Month", y = "Climb/distance ratio")
-
-
-st.subheader("Feet climbed and calories burned by week")
-st.line_chart(df_week2, x="Week")
-
-st.subheader("Ascent/distance ratio by week")
-st.line_chart(df_week, x="Week", y = "Climb/distance ratio")
-
-
-st.subheader("The pandas dataframes:")
-st.markdown("Yearly")
-st.write(df_year)
-st.markdown("Monthly")
-st.write(df_month)
-st.markdown("Weekly")
-st.write(df_week)
+st.subheader("Data by Week")
+tab1, tab2, tab3 = st.tabs(["Ascent & Calories", "Ascent/distance ratio", "Dataframe"])
+tab1.line_chart(df_week2, x="Week", height=400)
+tab2.line_chart(df_week, x="Week", y = "Climb/distance ratio", height=400)
+tab3.dataframe(df_week, height=400, use_container_width=True)
