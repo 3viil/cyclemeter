23db3df54ca78af92c98d640a4319b5b338ccfe0
3viil <48308226+3viil@users.noreply.github.com>
2025-05-31 11:06:48 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 df_year = pd.read_csv('Calendar-Years.csv')
 df_year = df_year[df_year['Activity'] == 'Cycle']
@@ -29,18 +28,18 @@ st.header(":blue[Cyclemeter MTB Data, 2011-2025]")
 
 st.subheader("Data by Year")
 tab1, tab2, tab3 = st.tabs(["Ascent & Calories", "Ascent/distance ratio", "Dataframe"])
-tab1.line_chart(df_year, x="Year", y = ['Ascent (feet)','Calories'], height=400)
-tab2.line_chart(df_year, x="Year", y = "Climb/distance ratio", height=400)
-tab3.dataframe(df_year, height=400, use_container_width=True)
+tab1.line_chart(df_year, x = "Year", y = ['Ascent (feet)','Calories'], height = 400)
+tab2.line_chart(df_year, x = "Year", y = "Climb/distance ratio", height = 400)
+tab3.dataframe(df_year, height = 400, use_container_width = True)
 
 st.subheader("Data by Month")
 tab1, tab2, tab3 = st.tabs(["Ascent & Calories", "Ascent/distance ratio", "Dataframe"])
-tab1.line_chart(df_month, x="Month", y = ['Ascent (feet)','Calories'], height=400)
-tab2.line_chart(df_month, x="Month", y = "Climb/distance ratio", height=400)
-tab3.dataframe(df_month, height=400, use_container_width=True)
+tab1.line_chart(df_month, x = "Month", y = ['Ascent (feet)','Calories'], height = 400)
+tab2.line_chart(df_month, x ="Month", y = "Climb/distance ratio", height = 400)
+tab3.dataframe(df_month, height = 400, use_container_width = True)
 
 st.subheader("Data by Week")
 tab1, tab2, tab3 = st.tabs(["Ascent & Calories", "Ascent/distance ratio", "Dataframe"])
-tab1.line_chart(df_week, x="Week", y = ['Ascent (feet)','Calories'], height=400)
-tab2.line_chart(df_week, x="Week", y = "Climb/distance ratio", height=400)
-tab3.dataframe(df_week, height=400, use_container_width=True)
+tab1.line_chart(df_week, x = "Week", y = ['Ascent (feet)','Calories'], height = 400)
+tab2.line_chart(df_week, x = "Week", y = "Climb/distance ratio", height = 400)
+tab3.dataframe(df_week, height = 400, use_container_width = True)
